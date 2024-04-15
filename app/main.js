6cee8acf1aa5a0548bdbb6a366b42d97f47f99e8
Rusted2361 <hamzasajid4040@gmail.com>
2024-04-16 02:48:32 +0500

    post file

--- app/main.js
+++ app/main.js
@@ -57,7 +57,7 @@ const server = net.createServer((socket) => {
       }
 
     }else if (method === "POST" && path.startsWith("/files")) {
-      let content = requestLines[request.length - 1];
+      let content = requestLines[requestLines.length - 1];
       const fileName = path.replace("/files/", "");
       const directory = process.argv[3] ?? __dirname;
       const filePath = pathModule.join(directory, fileName);
