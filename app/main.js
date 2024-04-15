4e1deb4994b91385074ea414481b6e7e629a0d8e
Rusted2361 <hamzasajid4040@gmail.com>
2024-04-15 20:09:26 +0500

    respond with 404

--- app/main.js
+++ app/main.js
@@ -5,12 +5,23 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-
   
   socket.on("data", (data) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    
+    const requestData = data.toString();
+    
+    const requestLines = requestData.split("\r\n");
+    
+    const [method, path, httpVersion] = requestLines[0].split(" ");
+    if (path == '/'){
+      const response = "HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(response);
+    }else{
+      const response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      socket.write(response);
+    }
     socket.end();
-    server.close();
+    //server.close();
   });
 });
 
